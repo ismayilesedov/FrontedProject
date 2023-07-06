74b588f0859daecb921d1858e159f79be9270c5b
ismayilras@code.edu.az <ismayilras@code.edu.az>
2023-07-06 18:24:54 +0400

    False

--- main_folder/script.js
+++ main_folder/script.js
@@ -1,8 +1,70 @@
 
 
 
+
 $(".card").hover(function () {
-
     $(".card").hide("products_icon", 1000)
-
 })
+
+
+
+
+
+
+let newBtn = document.querySelector(".new");
+let featuredBtn = document.querySelector(".featuredbutton");
+let topsellersBtn = document.querySelector(".topsellers");
+let myCards = document.querySelector(".all_products");
+let myCardsAgain = document.querySelector(".all_products_again")
+let featuredCard = document.querySelector(".featured")
+let TopSellersCard = document.querySelector(".TopSellers")
+
+featuredBtn.addEventListener("click", showFeature)
+newBtn.addEventListener("click", showNew)
+topsellersBtn.addEventListener("click", ShowTopSellers)
+
+function showFeature() {
+    featuredCard.classList.remove("featuredblock")
+    featuredBtn.style.color = "#0989ff"
+    removeTopsellers()
+    removeNew()
+
+}
+
+function showNew() {
+    myCards.classList.remove("mystyle")
+    myCardsAgain.classList.remove("mystyle");
+    featuredCard.classList.add("featuredblock")
+    newBtn.style.color = "#0989ff"
+    removeTopsellers()
+    removeFeature()
+}
+
+function ShowTopSellers() {
+    TopSellersCard.classList.remove("TopSellers")
+    topsellersBtn.style.color = "#0989ff"
+    removeNew()
+    removeFeature()
+}
+
+
+
+
+
+function removeTopsellers() {
+    TopSellersCard.classList.add("TopSellers")
+    topsellersBtn.style.color = "black"
+}
+function removeNew() {
+    myCards.classList.add("mystyle")
+    myCardsAgain.classList.add("mystyle");
+    newBtn.style.color = "black"
+}
+function removeFeature() {
+    featuredCard.classList.add("featuredblock")
+    featuredBtn.style.color = "black"
+}
+
+
+
+
