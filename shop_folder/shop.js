16953961757a084afcf91d5fe29ad7d78aa4c153
ismayilras@code.edu.az <ismayilras@code.edu.az>
2023-07-09 16:13:29 +0400

    responsive homepages

---
+++ shop_folder/shop.js
@@ -0,0 +1,15 @@
+//  <div class="range-filter">
+// <div class="range-controls">
+//     <div class="scale">
+//         <div style="margin-left:10px;width:150px;" class="bar"></div>
+//     </div>
+//     <div class="toggle min-toggle" style="left:40px;"></div>
+//     <div class="toggle max-toggle" style="left:180px;"></div>
+// </div>
+
+// </div>
+// <div class="filter_description">
+// <i class="fa-solid fa-box"></i>
+// <i class="fa-solid fa-grip-lines"></i>
+// <p>Showing 1–12 of 37 results</p>
+// </div> 
